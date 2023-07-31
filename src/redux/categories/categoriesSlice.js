e3b1a5554272b3452d637fd31d6e702dc1763ba8
devendra-alt <devendramulewa171@gmail.com>
2023-07-31 21:39:36 +0530

    Added status reducer in categoriesSlice.js

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -4,3 +4,15 @@ const initialState = {
   categories: [],
   status: 'Under construction',
 };
+
+export const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    checkStatus: (state) => state.status,
+  },
+});
+
+export const { checkStatus } = categoriesSlice.actions;
+
+export default categoriesSlice.reducer;
