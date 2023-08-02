ba355f9dfcf0b678c0a882b5ba3f8532e30d5c38
devendra-alt <devendramulewa171@gmail.com>
2023-08-02 22:49:48 +0530

    Added filter by category in bookSlice.js

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,20 +1,47 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  value: [],
+  value: [
+    {
+      item_id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
 };
 
 export const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    add: (state, action) => state.value.push(action.payload),
+    add: (state, action) => {
+      let newBook = action.payload;
+      state.value.push(newBook);
+    },
     remove: (state, action) => {
-      state.value = state.value.filter((book) => book.id !== action.payload);
+      let id = action.payload;
+      state.value = state.value.filter((book) => book.item_id !== id);
+    },
+    filterByCategory: (state, action) => {
+      let category = action.payload;
+      state.value = state.value.filter((book) => book.category === category);
     },
   },
 });
 
-export const { add, remove } = booksSlice.actions;
+export const { add, remove, filterByCategory } = booksSlice.actions;
 
 export default booksSlice.reducer;
