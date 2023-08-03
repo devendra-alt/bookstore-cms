0af5cd96fa71d2566d682f93121b5c603b41db84
devendra-alt <devendramulewa171@gmail.com>
2023-08-03 20:37:16 +0530

    Added reducer builder cases fro fetchBooks,addNewBook and removeBook asynThunk

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -36,47 +36,52 @@ export const removeBook = createAsyncThunk('books/removeBook', async (data) => {
 });
 
 const initialState = {
-  value: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+  value: [],
+  loading: 'idle',
 };
 
 const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    add: (state, action) => {
-      const newBook = action.payload;
-      state.value.push(newBook);
-    },
-    remove: (state, action) => {
-      const id = action.payload;
-      state.value = state.value.filter((book) => book.item_id !== id);
-    },
     filterByCategory: (state, action) => {
       const category = action.payload;
       state.value = state.value.filter((book) => book.category === category);
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.pending, (state) => {
+        state.loading = 'pending';
+      })
+      .addCase(fetchBooks.fulfilled, (state, action) => {
+        state.value = action.payload;
+        state.loading = 'succeded';
+      })
+      .addCase(fetchBooks.rejected, (state) => {
+        state.loading = 'failed';
+      })
+      .addCase(addNewBook.pending, (state) => {
+        state.loading = 'pending';
+      })
+      .addCase(addNewBook.fulfilled, (state) => {
+        state.loading = 'succeded';
+      })
+      .addCase(addNewBook.rejected, (state) => {
+        state.loading = 'failed';
+      })
+      .addCase(removeBook.pending, (state) => {
+        state.loading = 'pending';
+      })
+      .addCase(removeBook.fulfilled, (state) => {
+        state.loading = 'succeded';
+      })
+      .addCase(removeBook.rejected, (state) => {
+        state.loading = 'failed';
+      });
+  },
 });
 
-export const { add, remove, filterByCategory } = booksSlice.actions;
+export const { filterByCategory } = booksSlice.actions;
 
 export default booksSlice.reducer;
