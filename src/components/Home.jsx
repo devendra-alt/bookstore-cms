4d862aa42be734e2eb3466e2290e10216266b1b3
devendra-alt <devendramulewa171@gmail.com>
2023-08-03 20:34:05 +0530

    Added fetchBooks asyncThunk to book list component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Header from './Header';
 import Book from './Book';
 import Form from './Form';
+import { useEffect } from 'react';
+import { fetchBooks } from '../redux/books/booksSlice';
 
 export default function Home() {
   const books = useSelector((state) => state.books.value);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
   return (
     <>
       <Header />
